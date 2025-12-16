5d965838830e8fe2f1bb24d02a90a13f7edab77d
Ajinkya Dhadave <Ajinkya Dhadave@ho.rnt.ai>
2025-12-16 11:34:42 +0530

    Chnage HomeScreen Haeding commit

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -5,7 +5,7 @@ import { View, Text } from "react-native";
 export default function HomeScreen() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Voters List</Text>
+            <Text>Home Screen</Text>
         </View>
     );
 }
